7bb2f60060e88e4b99f2a0ea3fbe73b2cd647733
jcossio <jorge.cossio@yahoo.com>
2021-04-28 23:01:59 -0500

    Adicionando primera version de pagina de imagenes

--- Discography/Discography.Web/Pages/Album/View.cshtml.cs
+++ Discography/Discography.Web/Pages/Album/View.cshtml.cs
@@ -1,16 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Discography.Domain.Api;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace Discography.Web.Pages.Album
 {
     public class ViewModel : PageModel
     {
+        private readonly IPhotosApi photosApi;
+
+        /// <summary>
+        /// Model for the page
+        /// </summary>
+        public IEnumerable<Domain.Models.Photo> Photos { get; set; }
+
+        /// <summary>
+        /// Page constructor. Inject the api service
+        /// </summary>
+        /// <param name="photosApi">Photos API</param>
+        public ViewModel(IPhotosApi photosApi)
+        {
+            // Store it
+            this.photosApi = photosApi;
+        }
+
+        /// <summary>
+        /// Page get operation
+        /// </summary>
         public void OnGet()
         {
+            // Set the photos property for display
+            Photos = photosApi.GetPhotoDataByAlbum(1); //AlbumId);
         }
     }
 }
