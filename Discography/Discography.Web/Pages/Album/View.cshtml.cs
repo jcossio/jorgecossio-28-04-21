ce115ba3c4517c3f504ecf02d27e56078625f91b
jcossio <jorge.cossio@yahoo.com>
2021-04-29 00:45:58 -0500

    Add code as it is before deadline

--- Discography/Discography.Web/Pages/Album/View.cshtml.cs
+++ Discography/Discography.Web/Pages/Album/View.cshtml.cs
@@ -1,4 +1,5 @@
 using Discography.Domain.Api;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 
@@ -7,6 +8,7 @@ namespace Discography.Web.Pages.Album
     public class ViewModel : PageModel
     {
         private readonly IPhotosApi photosApi;
+        private readonly ICommentsApi commentsApi;
 
         /// <summary>
         /// Model for the page
@@ -17,19 +19,32 @@ namespace Discography.Web.Pages.Album
         /// Page constructor. Inject the api service
         /// </summary>
         /// <param name="photosApi">Photos API</param>
-        public ViewModel(IPhotosApi photosApi)
+        public ViewModel(IPhotosApi photosApi, ICommentsApi commentsApi)
         {
             // Store it
             this.photosApi = photosApi;
+            this.commentsApi = commentsApi;
         }
 
         /// <summary>
         /// Page get operation
         /// </summary>
-        public void OnGet()
+        public void OnGet(int? AlbumId)
         {
+            // [TODO] Not getting the Album Id at this moment
+
             // Set the photos property for display
-            Photos = photosApi.GetPhotoDataByAlbum(1); //AlbumId);
+            Photos = photosApi.GetPhotoDataByAlbum(1); // AlbumId);
+        }
+
+        /// <summary>
+        /// Get the comments for a specified photo
+        /// </summary>
+        /// <param name="photoId">Photo Id</param>
+        /// <returns></returns>
+        public ActionResult GetComments(int photoId)
+        {
+            return new OkObjectResult(commentsApi.GetCommentDataByPhoto(photoId));
         }
     }
 }
