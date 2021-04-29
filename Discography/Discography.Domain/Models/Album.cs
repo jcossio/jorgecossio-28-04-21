4541365b2be66b5fe04f952abcc6dec2f5b32887
jcossio <jorge.cossio@yahoo.com>
2021-04-28 21:09:03 -0500

    Adding domain project

---
+++ Discography/Discography.Domain/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discography.Domain.Models
+{
+    public class Album
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+}
